fa378cb54e4af553923dd29f35f08abe2fc1f5fa
Mustafa Tahir <mustafatahirhussein@gmail.com>
2023-08-25 19:26:12 +0500

    changes

--- lib/demo_screen.dart
+++ lib/demo_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher_demo/widgets/customized_button.dart';
 
 class DemoScreen extends StatelessWidget {
@@ -7,33 +8,100 @@ class DemoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Url Launcher"),
-      ),
-      body: ListView(
-        shrinkWrap: true,
-        padding: const EdgeInsets.symmetric(
-          horizontal: 14,
-          vertical: 30,
+        appBar: AppBar(
+          title: const Text("Url Launcher"),
         ),
-        children: [
+        body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 14,
+            vertical: 30,
+          ),
+          children: [
+            CustomizedButton(
+                onTap: () {
+                  emailRedirection();
+                },
+                label: "Send an Email"),
+            CustomizedButton(
+                onTap: () {
+                  smsRedirection();
+                },
+                label: "Send an SMS"),
+            CustomizedButton(
+                onTap: () {
+                  telRedirection();
+                },
+                label: "TEL"),
+            CustomizedButton(
+                onTap: () {
+                  urlRedirectInApp('https://www.google.com');
+                },
+                label: "Redirection In-App Preview"),
+            CustomizedButton(
+                onTap: () {
+                  urlRedirect("https://www.google.com");
+                },
+                label: "Redirection"),
+          ],
+        ));
+  }
 
-          CustomizedButton(onTap: () {
-          }, label: "Send an Email"),
+  dynamic urlRedirect(String url) async {
+    final result = await launchUrl(Uri.parse(url));
 
-          CustomizedButton(onTap: () {
-          }, label: "Send an SMS"),
+    if (!result) {
+      throw Exception("Error");
+    }
+  }
 
-          CustomizedButton(onTap: () {
-          }, label: "TEL"),
-
-          CustomizedButton(onTap: () {
-          }, label: "Redirection In-App Preview"),
-
-          CustomizedButton(onTap: () {
-          }, label: "Redirection"),
-        ],
-      )
+  dynamic urlRedirectInApp(String url) async {
+    final result = await launchUrl(
+      Uri.parse(url),
+      mode: LaunchMode.inAppWebView,
+      webViewConfiguration: const WebViewConfiguration(
+        enableJavaScript: true,
+      ),
     );
+
+    if (!result) {
+      throw Exception("Error");
+    }
+  }
+
+  String encodeQueryParams(Map<String, String> params) {
+    return params.entries
+        .map((MapEntry<String, String> e) =>
+            "${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}")
+        .join('&');
+  }
+
+  dynamic emailRedirection() async {
+    Uri emailRes = Uri(
+      scheme: 'mailto',
+      path: 'a@gmail.com',
+      query: encodeQueryParams({"subject": "Temporary email subject"}),
+    );
+
+    await launchUrl(emailRes);
+  }
+
+  dynamic smsRedirection() async {
+    Uri emailRes = Uri(
+      scheme: 'sms',
+      path: '033675756464',
+      query: encodeQueryParams({"body": "Temporary SMS subject"}),
+    );
+
+    await launchUrl(emailRes);
+  }
+
+  dynamic telRedirection() async {
+    Uri emailRes = Uri(
+      scheme: 'tel',
+      path: '033675756464',
+    );
+
+    await launchUrl(emailRes);
   }
 }
